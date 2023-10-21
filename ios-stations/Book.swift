9418521c28164c2868b16c29a2b18216fe725081
shake551 <73379887+shake551@users.noreply.github.com>
2023-10-21 16:08:53 +0900

    sta12

--- ios-stations/Book.swift
+++ ios-stations/Book.swift
@@ -4,4 +4,11 @@
 //
 
 struct Book: Decodable {
+    var id: String
+    var title: String
+    var url: String
+    var detail: String
+    var review: String
+    var reviewer: String
+    var isMine: Bool
 }
