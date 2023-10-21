2a9816fc3a5e30491daa93596b044fe8fc882bfc
shake551 <73379887+shake551@users.noreply.github.com>
2023-10-21 16:52:07 +0900

    sta14

--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -7,18 +7,10 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
-    @IBOutlet weak var button :UIButton!
-
     var books: [Book]?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func presentSecondViewController(_ sender: UIButton) {
-        let secondVC = SecondViewController(url: "https://techbowl.co.jp/")
-
-        self.present(secondVC, animated: true, completion: nil)
     }
 }
 
